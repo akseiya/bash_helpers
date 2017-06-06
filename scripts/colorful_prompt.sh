490e669999afcd26159c15dfb60d720b7af017f1
Michał Lech <akseiya@gmail.com>
2017-06-06 12:09:49 +0100

    moar colorful prompt

--- scripts/colorful_prompt.sh
+++ scripts/colorful_prompt.sh
@@ -1,12 +1,18 @@
+function ruby_version()
+{
+    ruby --version | awk '{print $2}'
+}
+
 function makeps()
 {
     creset="\[\033[0m\]"
     cuser=`colours.js yellow p`
-    chost=`colours.js cyan p`
+    chost=`colours.js purple p`
     cpwd=`colours.js cyan p`
     cgit=`colours.js green p`
+    cruby=`colours.js red p`
 
-    echo "[$cgit\$(git rev-parse --abbrev-ref HEAD 2> /dev/null)$creset] : $cpwd\$(pwd)$creset\n$cuser\u$creset @ $chost\h$creset "
+    echo "[$cgit\$(git rev-parse --abbrev-ref HEAD 2> /dev/null)$creset] : $cpwd\$(pwd)$creset\n$cuser\u$creset @ $chost\h$creset (${cruby}∇ \$(ruby_version)$cpwd ⬡ \$(node -v)${creset})\n> "
 }
 
 export PS1=—————————————————————————————————"\n"`makeps`
