7671c37e8d0e019ff8e74868398e1bb9f0ca3a3c
Michał Lech <akseiya@gmail.com>
2023-01-26 17:21:35 +0000

    added version reporting for python

--- scripts/colorful_prompt.sh
+++ scripts/colorful_prompt.sh
@@ -1,6 +1,9 @@
-function ruby_version()
-{
+___ruby_v() {
     ruby --version | awk '{print $2}'
+}
+
+___python_v() {
+   python -V |& cut -d ' ' -f 2
 }
 
 function makeps()
@@ -10,9 +13,13 @@ function makeps()
     chost=`colours.js purple p`
     cpwd=`colours.js cyan p`
     cgit=`colours.js green p`
-    cruby=`colours.js red p`
+    cruby=`colours.js red h p`
 
-    echo "[$cgit\$(git rev-parse --abbrev-ref HEAD 2> /dev/null)$creset] : $cpwd\$(pwd)$creset\n$cuser\u$creset @ $chost\h$creset (${cruby}∇ \$(ruby_version)$cpwd ⬡ \$(node -v)${creset})\n> "
+    echo -n " ${cruby}∇ \$(___ruby_v) "
+    echo "$cpwd⬡ \$(node -v)${creset} 🐍 ${cgit}\$(___python_v)$creset"
+    echo -n "[$cgit\$(git rev-parse --abbrev-ref HEAD 2> /dev/null || echo NO REPO)$creset]"
+    echo " : $cpwd\$(pwd)$creset"
+    echo " $cuser\u$creset @ $chost\h$creset > "
 }
 
 export PS1=—————————————————————————————————"\n"`makeps`
