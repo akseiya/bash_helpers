a5cdf9e788365db37bdc98dd095da9f824978091
Michał Lech <akseiya@gmail.com>
2024-08-02 17:46:07 +0200

    add directly to /etc/sudoers but only once

--- scripts/make_me_passwordless_sudoer.sh
+++ scripts/make_me_passwordless_sudoer.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
+sudo grep -q $USER /etc/sudoers && {
+    echo "/etc/sudoers already mentions you in the following line:"
+    sudo grep $USER /etc/sudoers
+    exit 1
+}
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
